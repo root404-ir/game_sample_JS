22b4388e882d0fe25ac44738611ed141ece99275
root404 <mohammad>
2024-05-17 14:06:58 +0330

    fixed score

--- 1_coins/app.js
+++ 1_coins/app.js
@@ -1,6 +1,6 @@
-const coinArray = ["Tails", "Heads"]
-const message = document.querySelector('.message')
-const buttons = document.querySelectorAll('button')
+const coinArray = ["Heads", "Tails"]
+const message = document.querySelector(".message")
+const buttons = document.querySelectorAll("button")
 let score = [0, 0]
 for (let i = 0; i < buttons.length; i++) {
     console.log(buttons[i]);
@@ -12,15 +12,15 @@ function tossCoin(e) {
     let computerGuess = coinArray[computerToss]
     message.innerHTML = " computer selected :" + computerGuess + "<br\>"
     let output
-    if (playerGuess === computerGuess) {
-        output = " Player Win. "
+    if (playerGuess < computerGuess) {
+        output = "player Win. "
         score[0]++
     }
     else {
-        output = " Computer win. "
+        output = "computer Win. "
         score[1]++
     }
-    message.innerHTML += output + " <br\> player : " + score[0] + " <br\> " + "Computer : " + score[1]
+    message.innerHTML = output + "<br\>" + "player : " + score[0] + "<br\>" + "computer : " + score[1]
     if (score[0] === 20 || score[1] === 20) {
         alert("Finish")
         if (score[0] > score[1]) {
@@ -35,3 +35,45 @@ function tossCoin(e) {
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function tossCoin(e) {
+//     let playerGuess = e.target.innerText
+//     let computerToss = Math.floor(Math.random() * 2)
+//     let computerGuess = coinArray[computerToss]
+//     message.innerHTML = " computer selected :" + computerGuess + "<br\>"
+//     let output
+//     if (playerGuess === computerGuess) {
+//         output = " Player Win. "
+//         score[0]++
+//     }
+//     else {
+//         output = " Computer win. "
+//         score[1]++
+//     }
+//     message.innerHTML += output + " <br\> player : " + score[0] + " <br\> " + "Computer : " + score[1]
+//     if (score[0] === 20 || score[1] === 20) {
+//         alert("Finish")
+//         if (score[0] > score[1]) {
+//             alert("players win")
+//         }
+//         else {
+//             alert("computer win")
+//         }
+//         if (score[0] > score[1] || score[0] < score[1]) {
+//             score[0] = null
+//             score[1] = null
+//         }
+//     }
+// }
