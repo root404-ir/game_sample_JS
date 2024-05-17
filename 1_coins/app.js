4fd97b3b15c39736be642870202ad4813d650598
root404 <mohammad>
2024-05-17 17:52:31 +0330

    fixed the socre

--- 1_coins/app.js
+++ 1_coins/app.js
@@ -25,7 +25,6 @@ function tossCoin(e) {
         alert("Finish")
         if (score[0] > score[1]) {
             window.alert("players win")
-            
         }
         else {
             window.alert("computer win")
@@ -33,48 +32,7 @@ function tossCoin(e) {
         if (score[0] > score[1] || score[0] < score[1]) {
             score[0] = null
             score[1] = null
+
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function tossCoin(e) {
-//     let playerGuess = e.target.innerText
-//     let computerToss = Math.floor(Math.random() * 2)
-//     let computerGuess = coinArray[computerToss]
-//     message.innerHTML = " computer selected :" + computerGuess + "<br\>"
-//     let output
-//     if (playerGuess === computerGuess) {
-//         output = " Player Win. "
-//         score[0]++
-//     }
-//     else {
-//         output = " Computer win. "
-//         score[1]++
-//     }
-//     message.innerHTML += output + " <br\> player : " + score[0] + " <br\> " + "Computer : " + score[1]
-//     if (score[0] === 20 || score[1] === 20) {
-//         alert("Finish")
-//         if (score[0] > score[1]) {
-//             alert("players win")
-//         }
-//         else {
-//             alert("computer win")
-//         }
-//         if (score[0] > score[1] || score[0] < score[1]) {
-//             score[0] = null
-//             score[1] = null
-//         }
-//     }
-// }
