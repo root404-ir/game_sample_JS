24a13f4088c107e46b083a40fc963c7fd603039f
root404 <mohammad>
2024-06-02 12:33:18 +0330

    fixed Somebugs and generate comment

--- 3-guess-number/app.js
+++ 3-guess-number/app.js
@@ -1,14 +1,20 @@
+//find the elements
 const gameArea = document.querySelector('.game')
 const startBtn = document.querySelector('.start')
 const message = document.querySelector('.message_header')
 let gamePlay = false
 let score = 0
+//set click event for button
 startBtn.addEventListener("click", function () {
     if (!gamePlay) {
         gamePlay = true
+        //When the game is reset، the inputs are null.
+        gameArea.innerHTML = null
         score = 0
-        maker()
+        //Here dynamically argument you can set the number of Harar inputs in real time
+        maker(5)
         startBtn.innerHTML = "check combo"
+        message.innerHTML = "Guess the Combo"
     } else {
         let numbers = document.querySelectorAll('.number_input')
         score++
@@ -25,25 +31,39 @@ startBtn.addEventListener("click", function () {
                 numbers[x].style.backgroundColor = color
                 numbers[x].style.color = "black"
             }
-            if(winCondition === numbers.length)
-                {
-                    console.log("gameFinished");
-                }
+            if (winCondition === numbers.length) {
+                gameEnd()
+            }
         }
     }
 })
-let maker = () => {
-    for (let i = 0; i < 5; i++) {
+let gameEnd = () => {
+    message.innerHTML = "You solved the combo in " + score + " guesses "
+    //restart the game
+    gamePlay = false
+    //text button changed to reser
+    startBtn.innerHTML = "reset"
+}
+
+//create input element //num_input parameter for dynamic input number
+let maker = (num_input) => {
+    //using for loop, because create 5 input type (number)
+    for (let i = 0; i < num_input; i++) {
         let element = document.createElement('input')
         element.setAttribute('type', 'number')
+        //minimum and maximum input
         element.max = 9
         element.min = 1
+        //input styles
         element.style.width = "50px"
         element.size = 1
+        //added the class
         element.classList.add('number_input')
+        //using math method and random method for generate random number
         element.random = Math.floor(Math.random() * 10)
         element.value = 0
         element.order = i
+        //append the input elemnt to div tag (game area)
         gameArea.appendChild(element)
     }
 }
